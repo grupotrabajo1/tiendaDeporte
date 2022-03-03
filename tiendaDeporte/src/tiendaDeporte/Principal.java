674c98822ac249c4c6fdf56f3e1dbf33277e6f63
Álvaro <alvaromarqcaz@gmail.com>
2022-03-03 13:52:15 +0100

    Últimos retoques en Principal

--- tiendaDeporte/src/tiendaDeporte/Principal.java
+++ tiendaDeporte/src/tiendaDeporte/Principal.java
@@ -7,20 +7,24 @@ public class Principal
 	{
 		Scanner sc=new Scanner(System.in);
 		Cliente[] cli=new Cliente[4];
-		Proveedores[] prove=new Proveedores[3]; //ToDo Pablo tiene que hacer esto
+		Proveedor[] prove=new Proveedor[3];
 		Articulo[] art=new Articulo[2];
 		for(int i=0;i<cli.length;i++) //Bucle FOR para generar los clientes
 		{
 			System.out.println("Client "+(i+1)+" will be authorized:");
+			cli[i]=new Cliente();
 			cli[i].darAlta();
 		}
 		for(int i=0;i<prove.length;i++) //Bucle FOR para generar los proveedores
 		{
-			//ToDo, necesito el código de Pablo otra vez para crear a los proveedores
+			System.out.println("Provider "+(i+1)+" will be authorized:");
+			prove[i]=new Proveedor();
+			prove[i].AltaProveedor();
 		}
 		for(int i=0;i<art.length;i++) //Bucle FOR para generar los artículos
 		{
 			System.out.println("Article"+(i));
+			art[i]=new Articulo();
 			System.out.println("Article "+i+ " Code:");
 			art[i].setCodigo(sc.nextLine());
 			System.out.println("Article "+i+" Description:");
@@ -29,5 +33,6 @@ public class Principal
 			art[i].setPrecio(sc.nextDouble());
 			sc.nextLine();
 		}
+		//Tras estos tres bucles ya tendríamos creados todos los objetos Cliente, Proveedor y Artículo
 	}
 }
