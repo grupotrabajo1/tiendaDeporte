988eb579b4a990d8b3e59d89c9d9739671a27c6a
Pedro <subigamer03@gmail.com>
2022-03-03 13:25:24 +0100

    Modificacion de getters and setters

--- tiendaDeporte/src/tiendaDeporte/Articulo.java
+++ tiendaDeporte/src/tiendaDeporte/Articulo.java
@@ -44,4 +44,27 @@ public class Articulo{
         return "Codigo:" + codigo+ ", descripcion: " + descripcion+", precio: " +precio;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
